d96d141be03d954565a3cf7cb13d51083ad2fd17
TaneriX07 <tanerivince@gmail.com>
2022-05-26 22:58:17 +0800

    Add a way to change background when clicking the cube

--- src/components/Panorama.js
+++ src/components/Panorama.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useLoader } from '@react-three/fiber';
 import * as THREE from 'three';
 import Box from './Box';
@@ -14,12 +15,25 @@ const backgrounds = [
 ];
 
 export default function Panorama() {
-  const background = useLoader(THREE.TextureLoader, backgrounds[0].url);
+  const [activeBackground, setActiveBackground] = useState(1);
+
+  const { url } = backgrounds.find(({ id }) => id === activeBackground);
+  const background = useLoader(THREE.TextureLoader, url);
 
   return (
-    <mesh>
-      <sphereBufferGeometry args={[500, 60, 40]} />
-      <meshBasicMaterial map={background} side={THREE.BackSide} />
-    </mesh>
+    <group>
+      <mesh>
+        <sphereBufferGeometry args={[500, 60, 40]} />
+        <meshBasicMaterial map={background} side={THREE.BackSide} />
+      </mesh>
+      <group
+        onClick={(e) => {
+          e.stopPropagation();
+          setActiveBackground(activeBackground === 1 ? 2 : 1);
+        }}
+      >
+        <Box />
+      </group>
+    </group>
   );
 }
