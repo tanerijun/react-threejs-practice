bca4a23a1e8f71faf08d11b28fd0a1dfd00d40d0
TaneriX07 <tanerivince@gmail.com>
2022-05-26 22:58:58 +0800

    Remove box from App.js to prevent multiple renders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import './App.css';
-import Box from './components/Box';
 import Controls from './components/Controls';
 import Panorama from './components/Panorama';
 
@@ -13,7 +12,6 @@ export default function App() {
         <Suspense fallback={null}>
           <Panorama />
         </Suspense>
-        <Box />
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
         <pointLight position={[-10, -10, -10]} />
